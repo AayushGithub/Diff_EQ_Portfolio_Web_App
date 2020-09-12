8b91946bd5f894ca8033573c9c96780bcdf776c5
Aayush Gandhi <66742440+AayushGithub@users.noreply.github.com>
2020-09-13 00:46:52 +0530

    build

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,6 @@
 module.exports = {
   someSidebar: {
-<<<<<<< HEAD
     Definitions: ['doc1','slope_field'],
-=======
-    Definitions: ['slope_field'],
->>>>>>> parent of 3b8b406... minor change check this
     Analysis: ['mdx'],
   },
 };
